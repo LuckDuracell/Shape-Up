ac4823b0d29176ed6094b5d0d03f0d154694e572
LuckDuracell <lukedru@yahoo.com>
2021-08-14 23:22:11 -0700

    Initial Commit

---
+++ Shape Up/Shape_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shape_UpApp.swift
+//  Shape Up
+//
+//  Created by Luke Drushell on 8/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Shape_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
